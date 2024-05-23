3ac24dc32bfa3130b34c154fe16a75b6ab426121
Gabriel 2000 <166823901+gbr12122000@users.noreply.github.com>
2024-05-23 14:07:56 -0300

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -17,3 +17,18 @@ elif opcao == '2':
     print(f"Resultado: {resultado}")
 elif opcao == '3':
     num1 = float(input("Digite o primeiro número: "))
+
+
+
+resultado = math.sin(math.radians(angulo))  # Convertendo o ângulo de graus para radianos
+    print(f"Seno: {resultado:.2f}")
+elif opcao == '7':
+    angulo = float(input("Digite o ângulo em graus: "))
+    resultado = math.cos(math.radians(angulo))  # Convertendo o ângulo de graus para radianos
+    print(f"Cosseno: {resultado:.2f}")
+elif opcao == '8':
+    angulo = float(input("Digite o ângulo em graus: "))
+    resultado = math.tan(math.radians(angulo))  # Convertendo o ângulo de graus para radianos
+    print(f"Tangente:{resultado:.2f}")
+else:
+    print("Opção inválida!")
