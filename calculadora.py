56f45a9149312a59dd2cabdf66d2d369ab140f59
luanamaria06 <159254931+luanamaria06@users.noreply.github.com>
2024-05-23 13:29:02 -0300

    update calculadora.py
    Update calculadora, parte 3 do código.

--- calculadora.py
+++ calculadora.py
@@ -7,3 +7,13 @@ print("4. Divisão")
 print("5. Raiz Quadrada")
 print("6. Seno")
 print("7. Cosseno")
+
+
+
+elif opcao == '2':
+    num1 = float(input("Digite o primeiro número: "))
+    num2 = float(input("Digite o segundo número: "))
+    resultado = num1 - num2
+    print(f"Resultado: {resultado}")
+elif opcao == '3':
+    num1 = float(input("Digite o primeiro número: "))
