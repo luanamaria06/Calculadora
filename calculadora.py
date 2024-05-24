7d4a2dc33388ff51b0198d0a210d064a97cfc982
Andynlx01 <andressaamaro495@gmail.com>
2024-05-24 06:57:44 -0300

    Part2 calculadora
    parte2

--- calculadora.py
+++ calculadora.py
@@ -1,1 +1,9 @@
-print('CALCULADORA')
+print("8. Tangente")
+opcao = input("Escolha uma operação (1/2/3/4/5/6/7/8): ")
+
+if opcao == '1':
+    num1 = float(input("Digite o primeiro número: "))
+    num2 = float(input("Digite o segundo número: "))
+    resultado = num1 + num2
+
+print(f"Resultado: {resultado}")
