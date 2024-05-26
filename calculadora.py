2bd906c5196285673aa7499b96bce632d7d58be3
luanamaria06 <159254931+luanamaria06@users.noreply.github.com>
2024-05-26 14:23:08 -0300

    finalizada

--- calculadora.py
+++ calculadora.py
@@ -1,3 +1,4 @@
+#Ellen
 import math
 print("Calculadora")
 print("Operações disponíveis:")
@@ -8,6 +9,7 @@ print("4. Divisão")
 print("5. Raiz Quadrada")
 print("6. Seno")
 print("7. Cosseno")
+#Andressa
 print("8. Tangente")
 
 opcao = input("Escolha uma operação (1/2/3/4/5/6/7/8): ")
@@ -18,7 +20,7 @@ if opcao == '1':
     resultado = num1 + num2
 
     print(f"Resultado: {resultado}")
-
+#Luana
 elif opcao == '2':
     num1 = float(input("Digite o primeiro número: "))
     num2 = float(input("Digite o segundo número: "))
@@ -36,27 +38,27 @@ elif opcao == '4':
     if num2 == 0:
         print("Erro: Divisão por zero!")
 
-
-#Ryan
-else:
+        #Ryan
+    else:
         resultado = num1 / num2
         print(f"Resultado: {resultado}")
 elif opcao == '5':
     num = float(input("Digite o número para calcular a raiz quadrada: "))
     resultado = math.sqrt(num) 
     print(f"Resultado: {resultado}")
+
 elif opcao == '6':
     angulo = float(input("Digite o ângulo em graus: "))
 
-
-
-
-resultado = math.sin(math.radians(angulo))  # Convertendo o ângulo de graus para Radianos
+    #Gabriel
+    resultado = math.sin(math.radians(angulo))  # Convertendo o ângulo de graus para Radianos
     print(f"Seno: {resultado:.2f}")
+    
 elif opcao == '7':
     angulo = float(input("Digite o ângulo em graus: "))
     resultado = math.cos(math.radians(angulo))  # Convertendo o ângulo de graus para Radianos
     print(f"Cosseno: {resultado:.2f}")
+
 elif opcao == '8':
     angulo = float(input("Digite o ângulo em graus: "))
     resultado = math.tan(math.radians(angulo))  # Convertendo o ângulo de graus para Radianos
